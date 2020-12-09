67d0cdefa4b1e6d9e11767799cb9bb20c62618bd
VVO04 <VVO04@WINDOWS-KO4S7OQ>
2020-12-09 15:50:57 +0700

    update

--- GrpcGreeter/Services/DocumentService.cs
+++ GrpcGreeter/Services/DocumentService.cs
@@ -3,27 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
+using GrpcGreeter.DatabaseHelper;
 using Microsoft.Extensions.Logging;
 namespace DocumentService
 {
     public class GrpcCrudService : DocumentService.DocumentServiceBase
     {
-        private readonly List<Document> _documents = new List<Document>();
-        private int idCount = 0;
         private readonly ILogger<GrpcCrudService> _logger;
         public GrpcCrudService(ILogger<GrpcCrudService> logger)
         {
             _logger = logger;
-            _documents.Add(new Document()
-            {
-                DocumentId = idCount++,
-                DocumentName = "test doc"
-            });
         }
         public override Task<Documents> GetDocument(Empty empty, ServerCallContext context)
         {
             Documents pl = new Documents();
-            pl.Items.AddRange(_documents);
+            DocumentDapper documentDapper = new DocumentDapper();
+            var result = documentDapper.GetDocument();
+            pl.Items.AddRange(result);
             return Task.FromResult(pl);
         }
     }
